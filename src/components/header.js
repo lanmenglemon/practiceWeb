8000719ff43ac68148c4c44fcbb8dd7ee4f17db5
orangehhh <haoliu47@gmail.com>
2018-04-18 22:15:43 -0400

    Header Modification

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
-import '../style/header.css'
-import linkedin from '../images/linkedin.png'
+import linkedin from '../images/linkedin.jpg'
 import cart from '../images/cart.png'
 import logo from '../images/logo.png'
 
 class Header extends Component {
   render() {
     return(
-        <header className="navbar navbar-expand navbar-light d-flex flex-sm-column flex-md-row bd-navbar">
-          <div className="container mx-auto d-flex">
-            <a class="navbar-brand mr-0" href="/">
-              <img src={logo} id="logo" />
-            </a>
-            <ul className="navbar-nav flex-row d-flex">
-              <li className="nav-item d-none d-sm-block my-auto mr-3">
-                <a className="nav-link" href="#"><span>kris@parallume.com</span></a>
-              </li>
-              <li className="nav-item d-none d-md-block my-auto mx-3">
-                <span>(408)757-1116</span>
-              </li>
-              <li className="nav-item d-none d-lg-block my-auto ml-3">
-                <a className="nav-link" href="https://www.linkedin.com/in/kristofer-haushalter-617508144/"
+        <header className="navbar navbar-expand navbar-light flex-row bd-navbar py-0">
+          <div className="container mx-auto align-items-center">
+            <div className="col-auto d-inline">
+              <a className="navbar-brand" href="/">
+                <img src={logo} id="logo" />
+              </a>
+              <span className="navbar-text ml-0">Bringing Color to Life</span>
+            </div>
+            <div className="col-auto offset-3">
+              <a className="nav-link" href="#">
+                <span className="navbar-text">kris@parallume.com</span>
+              </a>
+            </div>
+            <div className="col-auto">
+              <span className="navbar-text">(408)757-1116</span>
+            </div>
+            <div className="col-auto d-inline">
+              <a className="nav-link" href="https://www.linkedin.com/in/kristofer-haushalter-617508144/"
                 target="_blank" rel="noopener">
                 <img src={linkedin} id="linkedin" />
-                </a>
-              </li>
-            </ul>
+              </a>
+            </div>
           </div>
         </header>
     );
   }
 }
-
 export default Header;
--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,29 +1,28 @@
 import React, {Component} from 'react';
-import '../style/navigationBar.css'
 
 class NavigationBar extends Component {
   render() {
     return(
-        <header class="navbar navbar-expand navbar-light flex-column flex-md-row bd-navbar">
+        <nav className="navbar navbar-expand navbar-light flex-column flex-md-row bd-navbar py-0">
           <div className="container mx-auto d-flex">
-            <ul class="navbar-nav bd-navbar-nav flex-row mx-auto">
-              <li class="nav-item my-auto mr-5">Home</li>
-              <li class="nav-item my-auto mx-5">Overview</li>
-              <li class="nav-item my-auto mx-5">Instruments</li>
-              <li class="nav-item my-auto mx-5">Applications</li>
-              <li class="nav-item my-auto ml-5 dropdown">
-                <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#"
+            <ul className="navbar-nav bd-navbar-nav flex-row mx-auto">
+              <li className="nav-item my-auto mr-5">Home</li>
+              <li className="nav-item my-auto mx-5">Overview</li>
+              <li className="nav-item my-auto mx-5">Instruments</li>
+              <li className="nav-item my-auto mx-5">Applications</li>
+              <li className="nav-item my-auto ml-5 dropdown">
+                <a className="nav-item nav-link dropdown-toggle mr-md-2" href="#"
                   data-toggle="dropdown" aria-haspopup="true">
                   More</a>
-                <div class="dropdown-menu dropdown-menu-right">
-                  <a class="dropdown-item">1</a>
-                  <a class="dropdown-item">2</a>
-                  <a class="dropdown-item">3</a>
+                <div className="dropdown-menu dropdown-menu-right">
+                  <a className="dropdown-item">1</a>
+                  <a className="dropdown-item">2</a>
+                  <a className="dropdown-item">3</a>
                 </div>
               </li>
             </ul>
           </div>
-        </header>
+        </nav>
     );
   }
 }
