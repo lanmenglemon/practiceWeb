9b8e58cd6f579e025a55c4564683fae7fa42336b
Beibei Wang <beibeiwang.beibei@gmail.com>
2018-04-22 00:49:06 -0400

    technology

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const CHANGE_SELECTED_TAB = 'CHANGE_SELECTED_TAB';
 export const SEND_EMAIL = 'SEND_EMAIL';
 
+export const TEMPLATE_DIRECTORY = "../templates";
+export const SENDER_EMAIL_ADDRESS = "bw243@njit.edu";
+export const AWS_REGION = "eu-west-1";
+export const AWS_ACCESS_KEY_ID = "AKIAIIL7UOQECZHC27MA";
+export const AWS_SECRET_ACCESS_KEY="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";
+
 export function changeSelectedTab(selectedTab) {
     return {
         type: CHANGE_SELECTED_TAB,
@@ -8,7 +14,9 @@ export function changeSelectedTab(selectedTab) {
     };
 }
 
+
 export function sendEmail(values, callback) {
+
     console.log("email sent");
     return {
       type: SEND_EMAIL,
