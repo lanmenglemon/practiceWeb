6248b1625b73e4b27edc2ca13b5db162f30c2467
orangehhh <haoliu47@gmail.com>
2018-05-05 17:30:18 -0400

    node

--- app.js
+++ app.js
@@ -1,51 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const ejs = require('ejs');
+var express = require('express');
+var path = require('path');
+
+var indexRouter = require('./routes/server');
 
 var app = express();
-app.use(logger('dev'));//命令行里显示请求
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-require('./routes/server')(app);
-const port = 3000;
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// view engine setup
-app.set('views', path.join(__dirname, '../client/dist'));
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/', indexRouter);
 
-
-
-app.all("/*", function(req, res) {
-    res.render("index");
-})
-
-app.listen(port, function() {
-    console.log("server is running on port 3000");
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-
-});
+app.listen(3000);
 
 module.exports = app;
