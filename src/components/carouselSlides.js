242d4d37519ef6d8866f86fb9b307e8c463d6355
Beibei Wang <beibeiwang.beibei@gmail.com>
2018-05-12 11:51:43 -0400

    CarouselUpdated

--- src/components/carouselSlides.js
+++ src/components/carouselSlides.js
@@ -84,6 +84,7 @@ class CarouselSlides extends Component {
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
           </ol>
           <div className="carousel-inner" role="listbox">
             {this.renderCarouselList()}
@@ -108,6 +109,7 @@ class CarouselSlides extends Component {
                   <li data-target="#carouselExampleIndicator" data-slide-to="1"></li>
                   <li data-target="#carouselExampleIndicator" data-slide-to="2"></li>
                   <li data-target="#carouselExampleIndicator" data-slide-to="3"></li>
+                  <li data-target="#carouselExampleIndicator" data-slide-to="4"></li>
                 </ol>
                 <div className="carousel-inner">
                   {this.renderModalList()}
